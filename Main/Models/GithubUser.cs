6e16b6455dc560468dfcffd7b50ffe29eb93e7da
Jeffrey Johnson <jefjohms@gmail.com>
2016-04-25 13:41:31 -0700

    Add project files.

---
+++ Main/Models/GithubUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Main.Models
+{
+    public class GithubUser
+    {
+        public string Login { get; set; }
+        public int Id { get; set; }
+        public string AvatarUrl { get; set; }
+        public string url { get; set; }
+        public string FollowersUrl { get; set; }
+        public string repos_url { get; set; }
+        public string GistsUrl { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ TestFoo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string url = "https://api.github.com/zen";
+
+            // *** Establish the request
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            // *** Set properties
+            //webRequest.Timeout = 10000;     // 10 secs
+            webRequest.UserAgent = "anonymous";
+
+            // *** Retrieve request info headers
+            try
+            {
+                HttpWebResponse loWebResponse = (HttpWebResponse) webRequest.GetResponse();
+                Encoding enc = Encoding.GetEncoding(1252);  // Windows default Code Page
+
+                StreamReader loResponseStream =
+                   new StreamReader(loWebResponse.GetResponseStream(), enc);
+
+                string lcHtml = loResponseStream.ReadToEnd();
+
+                loWebResponse.Close();
+                loResponseStream.Close();
+
+                Console.WriteLine(lcHtml);
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           
+
+            Console.Read();
+        }
+    }
+}
