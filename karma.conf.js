0f19a43e901c0d6d5baaba1a5ce329ec99ec88a7
Jeffrey Johnson <jefjohms+git@gmail.com>
2024-01-04 17:11:09 -0800

    Create build pipeline using GitHub Actions.

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-﻿module.exports = function (config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -15,6 +15,7 @@
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
+
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -35,7 +36,7 @@
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true,
     listenAddress: 'localhost',
     hostname: 'localhost'
